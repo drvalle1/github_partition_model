1ad541697eb1150d6aa6b0e27abaa3e7b5904586
drvalle@ufl.edu <drvalle@ufl.edu>
2017-04-28 09:39:20 -0400

    Commit 1
    Commit 1

---
+++ gibbs sampler functions.R
@@ -0,0 +1,97 @@
+assign.centroid=function(centroid,dist.mat){
+  dist1=dist.mat[,centroid]
+  res=rep(NA,nloc)
+  for (i in 1:nloc){
+    target=dist1[i,]
+    ind=which(target==min(target))
+    res[i]=centroid[ind]
+  }
+  res
+}
+#--------------------
+get.likelihood=function(dat,region){
+  dat1=merge(dat,region,all=T); dim(dat); dim(dat1)
+  dat1$n=1
+  dat2=aggregate(cbind(microsc1,n)~centroid,data=dat1,sum)
+  p1=lgamma(dat2$microsc1+phi.a)+
+     lgamma(dat2$n-dat2$microsc1+phi.b)-
+     lgamma(phi.a+dat2$n+phi.b)
+  phi=rbeta(nrow(dat2),dat2$microsc1+phi.a,dat2$n-dat2$microsc1+phi.b)
+  list(logl=sum(p1),phi=phi)
+}
+#--------------------
+update.regions=function(param){
+  region.o=param$region.map
+  centroid.o=sort(unique(region.o$centroid))
+  nreg=length(centroid.o)
+
+  if (nreg==1){
+    centroid.n=birth(centroid.o)
+    pdeath=(1/3)
+    pbirth=1
+    lpjump=log(pdeath/pbirth)
+  }
+  if (nreg==max.regions){
+    centroid.n=death(centroid.o)
+    pdeath=1
+    pbirth=(1/3)
+    lpjump=log(pbirth/pdeath)
+  }
+  if (nreg!=1 & nreg!=max.regions){
+    rv=runif(1)
+    if (rv < 1/3){
+      centroid.n=birth(centroid.o)
+      pdeath=(1/3)
+      if (nreg+1==max.regions) pdeath=1
+      pbirth=(1/3)
+      lpjump=log(pdeath/pbirth)
+    }
+    if (rv > 1/3 & rv < 2/3){
+      centroid.n=death(centroid.o)
+      pdeath=(1/3)
+      pbirth=1/3
+      if (nreg-1==1) pbirth=1
+      lpjump=log(pbirth/pdeath)
+    }
+    if (rv > 2/3){
+      centroid.n=swap(centroid.o)
+      lpjump=0
+    }
+  }
+  tmp=assign.centroid(centroid.n,dist.mat)
+  region.n=data.frame(loc.id=1:nloc,centroid=tmp)
+  
+  tmp=get.likelihood(dat,region.o)
+  pold=tmp$logl
+  phi.old=tmp$phi
+  
+  tmp=get.likelihood(dat,region.n)
+  pnew=tmp$logl
+  phi.new=tmp$phi
+  
+  prob=exp(pnew+lpjump-pold)
+  rv=runif(1)
+  if (rv<prob) return(list(region=region.n,prob=pnew,phi=phi.new))
+  return(list(region=region.o,prob=pold,phi=phi.old))
+}
+#----------------------------------------
+death=function(indinz){
+  sort(sample(indinz,size=length(indinz)-1))
+}
+#---------------------------------------------------------------------------------------------------
+swap=function(indinz){
+  tmp=1:nloc
+  indout=tmp[!tmp%in%indinz]
+  
+  keep=sample(indinz,size=length(indinz)-1)  
+  include=sample(indout,size=1)
+  sort(c(keep,include))
+}
+#---------------------------------------------------------------------------------------------------
+birth=function(indinz){
+  tmp=1:nloc
+  indout=tmp[!tmp%in%indinz]
+  include=sample(indout,size=1)
+  sort(c(indinz,include))
+}
+#---------------------------------------------------------------------------------------------------
---
+++ gibbs sampler.R
@@ -0,0 +1,63 @@
+rm(list=ls(all=TRUE))
+set.seed(6)
+
+setwd('U:\\independent studies\\partition models\\no covariates')
+source('gibbs sampler functions.R')
+dat=read.csv('fake data.csv',as.is=T)
+
+max.regions=100
+n.regions=50
+uni.loc=unique(dat[,c('loc.id','LATNUM','LONGNUM')])
+uni.loc=uni.loc[order(uni.loc$loc.id),]
+nloc=nrow(uni.loc)
+
+dist.mat=data.matrix(dist(uni.loc[,c('LONGNUM','LATNUM')]))
+# x2=(uni.loc$LONGNUM[2]-uni.loc$LONGNUM[1:5])^2
+# y2=(uni.loc$LATNUM[2] -uni.loc$LATNUM[1:5])^2
+# sqrt(x2+y2)
+# dist.mat[2,1:5]
+
+centroid=sort(sample(1:nloc,size=n.regions))
+tmp=assign.centroid(centroid,dist.mat)
+region.map=data.frame(loc.id=1:nloc,centroid=tmp)
+#---------------------------------------
+#priors
+
+phi.a=phi.b=1
+#---------------------------------------
+ngibbs=10000
+vec.logl=matrix(NA,ngibbs,1)
+vec.n=matrix(NA,ngibbs,1)
+vec.centroids=matrix(NA,ngibbs,max.regions)
+vec.phi=matrix(NA,ngibbs,max.regions)
+
+param=list(region.map=region.map)
+max.logl=-Inf
+for (i in 1:ngibbs){
+  #how many distinct regions?
+  z=unique(param$region.map$centroid)
+  n=length(z)
+  print(c(i,n))
+  
+  tmp=update.regions(param)
+  param$region.map=tmp$region
+  
+  vec.logl[i]=tmp$prob
+  vec.n[i]=n
+  
+  ind=1:length(tmp$phi)
+  vec.centroids[i,ind]=sort(unique(param$region.map$centroid))
+  vec.phi[i,ind]=tmp$phi
+  
+  if (tmp$prob>max.logl) {
+    max.logl=tmp$prob
+    fim=param$region.map
+  }
+}
+
+seq1=2000:ngibbs
+plot(vec.logl[seq1],type='l')
+
+setwd('U:\\independent studies\\partition models\\no covariates\\results')
+write.csv(vec.centroids[seq1,],'centroids.csv',row.names=F)
+write.csv(vec.phi[seq1,],'phi.csv',row.names=F)
